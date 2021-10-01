32551ce30e406933d9a41cda2b399fe9efab4f51
Idriss Neumann <idriss.neumann@comwork.io>
2021-10-02 00:32:26 +0200

    Use an api for airports instead

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,6 +3,6 @@
 echo "AMADEUS_API_KEY=${AMADEUS_API_KEY}" > .env
 echo "AMADEUS_API_SECRET=${AMADEUS_API_SECRET}" >> .env
 echo "API_URL=${API_URL}" >> .env
-echo "API_URL_INSIDE_CONTAINER=${API_URL}"
+echo "API_URL_INSIDE_CONTAINER=${API_URL}" >> .env
 
 docker-compose up -d --force-recreate
