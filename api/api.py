a93ff87129084223be688e4f20e60814c669174a
Idriss Neumann <idriss.neumann@comwork.io>
2021-12-18 13:07:39 +0100

    Remove cors app, already handled by reverse proxy

--- api/api.py
+++ api/api.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restful import Api
-from flask_cors import CORS
 
 from api_root import RootEndPoint
 from api_manifest import ManifestEndPoint
@@ -8,7 +7,6 @@ from api_amadeus import AmadeusEndPoint
 from api_airports import AirportsEndPoint
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 health_check_routes = ['/', '/health', '/health/', '/v1/health', '/v1/health/']
