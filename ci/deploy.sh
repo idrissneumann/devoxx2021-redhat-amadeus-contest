2c444939251e9102aced740aaa20c75edec9c89b
Idriss Neumann <neumann.idriss@gmail.com>
2021-12-26 13:43:52 +0000

    Update ci/deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,4 +5,6 @@ echo "AMADEUS_API_SECRET=${AMADEUS_API_SECRET}" >> .env
 echo "API_URL=${API_URL}" >> .env
 echo "API_URL_INSIDE_CONTAINER=${API_URL}" >> .env
 
+docker rmi -f "comworkio/devoxxfr-amadeus-api:latest" || :
+docker rmi -f "comworkio/devoxxfr-amadeus-ui-nginx:latest" || :
 docker-compose up -d --force-recreate
